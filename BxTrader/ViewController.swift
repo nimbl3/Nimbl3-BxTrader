de31e968ba9acbd17f52471e3627d3592a88ab45
Issarapong Poesua <dekablade01@gmail.com>
2017-09-21 16:01:22 +0700

    Initial Commit

---
+++ BxTrader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BxTrader
+//
+//  Created by Issarapong Poesua on 9/21/2560 BE.
+//  Copyright © 2560 Issarapong Poesua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
